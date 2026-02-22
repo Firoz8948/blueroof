f79bce7907078c460b69f445450986f1dd666d3f
firoz8948 <firoz8948@github.com>
2026-02-22 17:00:56 +0530

    google tag inserted

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -85,6 +85,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17969138073" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17969138073');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
